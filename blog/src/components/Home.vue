c00e7810ebacdaac8704f0c4b1aa4844505d4614
holicAZ <ajm4232@gmai.com>
2022-01-07 16:55:46 +0900

    day2 vueStudy

---
+++ blog/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  
+    <div class="container mt-4">
+    <h5>Vue 개발자의 블로그 입니다.</h5>
+    <p>vue 제작</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div  v-for="(글,i) in 블로그글" :key="i">
+      <router-link to="/detail">
+        <h5>{{글.title}}</h5>
+      </router-link>
+      <p>{{글.content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "List",
+    props:{
+      블로그글 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>

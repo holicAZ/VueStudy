d3db16058f0b23ac7f53c36e3802044ea6935d48
holicAZ <ajm4232@gmai.com>
2022-01-10 17:16:35 +0900

    day2 vueStudy(insta)

---
+++ insta/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li @click="step--">Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step==1" @click="step++">Next</li>
+      <li v-if="step==2" @click="publish"> 발행 </li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container @text="new_content = $event" :insta_data="insta_data" :step="step" :image_url="image_url"/>
+  <button @click="more"> 더보기 </button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
+
+ <div style="margin-top : 500px;"></div>
+</template>
+
+<script>
+import Container from './components/Container.vue';
+import insta_data from './assets/insta_data.js';
+import axios from 'axios';
+export default {
+  name: 'App',
+  data(){
+    return{
+      insta_data : insta_data,
+      more_cnt : 0,
+      step: 0,
+      image_url : "",
+      new_content : "",
+    }
+  },
+  components: {
+    Container:Container,
+  },
+  methods: {
+    publish(){
+      var new_data = {
+        name: "Kim Hyun",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.image_url,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: this.new_content,
+        filter: "perpetua"
+      };
+      this.insta_data.unshift(new_data); // unshift 맨 왼쪽(처음)에 자료 추가
+      this.step = 0;
+    }
+    ,
+    more(){
+      //axios.post('URL', {name:'kim'}).then().catch((err)=>{
+        
+      //}) 실패시 catch 내용 실행
+      axios.get('https://codingapple1.github.io/vue/more'+this.more_cnt+'.json')
+      .then(result =>{
+        console.log(this.more_cnt);
+        this.more_cnt++;
+        this.insta_data.push(result.data);  
+      })
+    },
+    upload(e){
+      let file = e.target.files;
+      console.log(file[0]);
+      let url = URL.createObjectURL(file[0]);
+      console.log(url);
+      this.step++;
+      this.image_url = url;
+    },
+  }
+}
+</script>
+
+<style>
+@import 'style.css';
+
+
+</style>
---
+++ insta/src/components/Container.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div v-if="step == 0">
+        <Post :insta = "insta" v-for="(insta,i) in insta_data" :key="i"/>
+    </div>
+    
+    <!-- 필터선택페이지 -->
+    <div v-if="step == 1">
+        <div class="upload-image" :style="{backgroundImage:'url('+image_url +')'}"></div>
+        <div class="filters">
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+            <div class="filter-1"></div>
+        </div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div v-if="step==2">
+        <div class="upload-image" :style="{backgroundImage:'url('+image_url +')'}"></div>
+        <div class="write">
+            <textarea class="write-box" @input="$emit('text',$event.target.value)" >write!</textarea>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from '../components/Post'
+export default {
+    data(){
+        return{
+            text:"",
+        }
+    },
+    components: {
+        Post,
+    },
+    props: {
+        insta_data: Array,
+        step: Number,
+        image_url: String,
+        publish_check: Boolean,
+    },
+}
+</script>
+
+<style>
+.upload-image{
+width: 100%;
+height: 450px;
+background: cornflowerblue;
+background-size : cover;
+}
+.filters{
+overflow-x:scroll;
+white-space: nowrap;
+}
+.filter-1 {
+width: 100px;
+height: 100px;
+background-color: cornflowerblue;
+margin: 10px 10px 10px auto;
+padding: 8px;
+display: inline-block;
+color : white;
+background-size: cover;
+}
+.filters::-webkit-scrollbar {
+height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+background: #f1f1f1; 
+}
+.filters::-webkit-scrollbar-thumb {
+background: #888; 
+border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+background: #555; 
+}
+.write-box {
+border: none;
+width: 90%;
+height: 100px;
+padding: 15px;
+margin: auto;
+display: block;
+outline: none;
+}
+ 
+</style>

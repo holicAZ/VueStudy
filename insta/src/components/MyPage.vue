eef4d6e89a32e0a8ad105422ec2d5d9a35d8a61d
holicAZ <ajm4232@gmail.com>
2022-01-13 17:03:25 +0900

    day4 vueStudy(composition API 및 검색)

--- insta/src/components/MyPage.vue
+++ insta/src/components/MyPage.vue
@@ -1,42 +1,68 @@
 <template>
 <div style="padding : 10px">
   <h4>팔로워</h4>
-  <input placeholder="Searh" />
+  <input placeholder="Search..." @input="searchWord = $event.target.value"/> 
   <div class="post-header" v-for="follow in follower" :key="follow">
     <div class="profile" :style="{backgroundImage: 'url(' + follow.image + ')'}"></div>
     <span class="profile-name">{{follow.name}}</span>
+
   </div>
 </div>
 </template>
 
 <script>
-import { onMounted,ref } from 'vue'; // vue 의 자동 랜더링은 ref 데이터 타입 덕분
+import { onMounted,ref,reactive,watch, } from 'vue'; // vue 의 자동 랜더링은 ref 데이터 타입 덕분
 import axios from 'axios';
+import {useStore} from 'vuex'
 export default {
     name : 'mypage',
     // json 데이터 axios로 가져오면 자동으로 object array로 변경해줌
+    
     setup(){ //compositon api 개발
         let follower = ref([]); // 변경을 실시간으로 반영하기 위해
-        
-        onMounted(()=>{ // composition api 에서 lifecycle Hook 사용하려면 On을 붙이고 ()=> 형식으로 사용
+        let followerIndex = ref([]);
+        let test = reactive({name : 'kim'}) // 보통 array, object 집어넣음
+        let searchWord = ref("");
+        let followerName = ref([]);
+        test;
+        //let prp = toRefs(props); // 그냥 사용하면 반응형이 아님. ref로 사용
+        watch(searchWord, ()=>{
+          followerName.value = follower.value.filter(searchTarget => 
+            searchTarget.name.includes(searchWord.value)
+          )
+          follower.value = followerName.value;
+          console.log(followerName.value);
+        });
 
-        })
+        let store = useStore();
+        console.log(store.state.name);
 
         axios.get('/follower.json').then((a)=>{
-            follower.value = a.data;
+          follower.value = a.data;
+          console.log("axios");
+          // follower.value.forEach(element=> {
+          //   followerName.value.push(element.name);
+          // })
+          // console.log(followerName.value);
         })
 
-        return {follower}
+        onMounted(()=>{ // composition api 에서 lifecycle Hook 사용하려면 On을 붙이고 ()=> 형식으로 사용
+           
+        })
+
+        
+      
+        return {follower, searchWord, followerName, followerIndex}
     },
     data(){
         return{
             
         }
     },
-    
+
 }
 </script>
-
-<style>
+<!-- scoped vue 전염 막음. -->
+<style scoped>
 
 </style>

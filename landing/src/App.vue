0f9a5eca0f9d707151cfc6b3697da268ef2c23dc
holicAZ <ajm4232@gmail.com>
2022-01-24 17:20:10 +0900

    landing(background-position add)

--- landing/src/App.vue
+++ landing/src/App.vue
@@ -383,6 +383,7 @@ section.black {
   left: 0;
   background-image: url('./assets/image/cake12.jpg');
   background-size: cover;
+  background-position: 50% 50%;
   width: 100%;
   height: 100%;
   opacity : 0.79;
